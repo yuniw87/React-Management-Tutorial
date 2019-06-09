ba7677f00e8e9fca5c996aae2e2ec7022e2c7735
Inwon Yun <iw@Inwons-MacBook-Pro.local>
2019-06-09 23:36:06 +0900

    Create Customer Database Table & Node.js Database Connection Setting

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +8,26 @@ const port = process.env.port || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id' : '1',
-            'image' : 'https://placeimg.com/64/64/any',
-            'name' : '홍길동',
-            'birthday': '961222',
-            'gender': '남자',
-            'job': '대학생'
-          },
-          {
-            'id' : '2',
-            'image' : 'https://placeimg.com/64/64/any',
-            'name' : '아무게',
-            'birthday': '971122',
-            'gender': '남자',
-            'job': '대학생'
-          },
-          {
-            'id' : '3',
-            'image' : 'https://placeimg.com/64/64/any',
-            'name' : '뭐라는거',
-            'birthday': '901512',
-            'gender': '남자',
-            'job': '대학생'
-          }
-    ]);
+    connection.query(
+      "select * from customer",
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    )
 });
 
 app.listen(port, () => console.log(`Listening on Port ${port}`));
